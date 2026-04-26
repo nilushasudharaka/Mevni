beab3c8d1c67a5bdf57fd1c34a93befd69261f72
Nilusha Sudharaka <nilushasudharaka24@gmail.com>
2026-04-26 13:05:03 +0530

    first commite

---
+++ backend/models/Item.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    percentage : {
+      type: Number,
+      required: [true, "Percentage is required"],
+      min: [0, "Percentage cannot be negative"],
+      max: [100, "Percentage cannot exceed 100"],
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
